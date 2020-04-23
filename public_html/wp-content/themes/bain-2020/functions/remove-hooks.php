2c811b5eb7e1d01467e8da479c560263c71e93d9
markbain <markbaindesign@gmail.com>
2020-04-23 19:42:26 +0200

    Update header logo and toggle

--- public_html/wp-content/themes/bain-2020/functions/remove-hooks.php
+++ public_html/wp-content/themes/bain-2020/functions/remove-hooks.php
@@ -13,6 +13,11 @@
  * 
  */
 
+
+
+remove_action('baindesign324_header_top', 'baindesign324_toggle_search', 60);
+remove_action('baindesign324_header_top', 'baindesign324_search_bar', 70);
+remove_action('baindesign324_header_top', 'baindesign324_menu_standard', 30);
 remove_action( 'wp_enqueue_scripts',   'bd324_enqueue_fp_styles',                      999 );
 remove_action('baindesign324_header_top', 'baindesign324_mmenu_toggle', 40);
 remove_action('baindesign324_header_top', 'baindesign324_mmenu_toggle_static', 40);
@@ -22,3 +27,5 @@ remove_action('baindesign324_pre_header', 'baindesign324_menu_account', 50);
 remove_action('baindesign324_pre_header', 'baindesign324_pre_header_wrapper_close', 100);
 remove_action('baindesign324_pre_header', 'baindesign324_pre_header_wrapper_open', 10);
 remove_action('baindesign324_pre_header', 'baindesign324_social_links', 60);
+remove_action('baindesign324_colophon', 'baindesign324_site_info_design_credit', 30);
+remove_action('baindesign324_colophon', 'baindesign324_back_to_top', 60);
