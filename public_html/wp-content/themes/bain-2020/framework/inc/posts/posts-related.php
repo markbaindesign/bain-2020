4c03ec0b3bc64ee274dd5a5feb89bf4550b0a4e4
markbain <markbaindesign@gmail.com>
2020-04-28 18:35:41 +0200

    Update related posts so it works with projects

--- public_html/wp-content/themes/bain-2020/framework/inc/posts/posts-related.php
+++ public_html/wp-content/themes/bain-2020/framework/inc/posts/posts-related.php
@@ -1,64 +1,115 @@
 <?php
 
-if ( ! function_exists( 'baindesign324_related_blog_posts' ) ) :
-	/**
-	 * Show a set of related posts based on tags
-	 */
+/**
+ * Show a set of related posts based on tags
+ */
 
-	function baindesign324_related_blog_posts() {
-		global $post;  
-	 	$orig_post = $post;
+if (!function_exists('baindesign324_related_blog_posts')) :
+   function baindesign324_related_blog_posts()
+   {
+      global $post;
+      $orig_post = $post;
+      $my_query = bd324_get_related_posts();
+      $header = bd324_related_posts_section_header();
 
-	 	// Only show related posts on single posts
-	 	if ( ! is_single() ) {
-	 		return;
-	 	}
+      if ($my_query->have_posts()) :
+         $classes = array(
+            'posts',
+            'posts--related',
+         );
+         baindesign324_generic_wrapper(NULL, $classes, NULL);
+         echo '<header><h3>';
+         _e($header, '_baindesign');
+         echo '</h3></header>';
+         echo '<div class="posts__wrapper">';
+         while ($my_query->have_posts()) {
+            $my_query->the_post();
+            get_template_part('content', 'archive');
+         }
+         echo '</div>';
+         baindesign324_generic_wrapper(NULL, NULL, 'close');
+      endif;
+      // Reset query
+      $post = $orig_post;
+      wp_reset_query();
+   }
+endif;
 
-		$custom_taxterms = wp_get_object_terms( 
-			$post->ID, 'post_tag', array(
-				'fields' => 'ids'
-			) 
-		);
-		 
-		$args = array(  
- 			'post_type' => array(
- 				'award',
- 				'book',
- 				'interview',
- 				'project',
- 				'post',
- 				'talk',
- 				'film',
-			),
-			'post__not_in'			=> array( 
-				$post->ID 
-			),  
-			'posts_per_page'		=> 5, 
-			'ignore_sticky_posts' 	=> 1,
-			'tax_query' => array(
-			    array(
-			        'taxonomy' => 'post_tag',
-			        'field' => 'id',
-			        'terms' => $custom_taxterms
-			    )
-			),
-		);  
-	
-		$my_query = new wp_query( $args );
+/**
+ * Get related posts
+ */
+if (!function_exists('bd324_get_related_posts')) :
+   function bd324_get_related_posts()
+   {
+      $id = get_the_ID();
+      $post_type = get_post_type($id);
 
-		if  ( $my_query->have_posts() ) : ?>
-			<div class="posts posts--related">
-				<div class="container">
-					<header><h3><?php _e('Related','_baindesign'); ?></h3></header>
-					<div class="posts__wrapper">
-						<?php while( $my_query->have_posts() ) {
-				    		$my_query->the_post();
-							get_template_part('content', 'archive');
-						} ?>
-					</div><!-- ..container -->
-				</div><!-- ..container -->
-			</div><!-- #related-blog-posts .section -->	
-		<?php endif;					
-		$post = $orig_post; wp_reset_query();
-	}
+      if ($post_type == 'project') {
+         $tax = 'skill';
+      } else {
+         $tax = 'post_tag';
+      }
+
+      $custom_taxterms = wp_get_object_terms(
+         $id,
+         $tax,
+         array(
+            'fields' => 'ids'
+         )
+      );
+
+      $args = array(
+         'post_type' => $post_type,
+         'post__not_in' => array(
+            $post->ID
+         ),
+         'posts_per_page'           => 3,
+         'ignore_sticky_posts'      => 1,
+         'tax_query' => array(
+            array(
+               'taxonomy' => $tax,
+               'field' => 'id',
+               'terms' => $custom_taxterms
+            )
+         ),
+      );
+
+      $related_query = new wp_query($args);
+      return $related_query;
+   }
+endif;
+
+/**
+ * Show the related posts
+ * 
+ * Outputs a section with related posts
+ */
+if (!function_exists('bd324_show_related_posts')) :
+   function bd324_show_related_posts()
+   {
+      // Check for related posts
+      $related = '';
+
+      // Show related posts if found
+      if ($related) {
+      }
+   }
+endif;
+
+/**
+ * Related Posts Section Header
+ */
+if (!function_exists('bd324_related_posts_section_header')) :
+   function bd324_related_posts_section_header()
+   {
+      $id = get_the_ID();
+      $post_type = get_post_type($id);
+
+      if ($post_type == 'project') {
+         $header = 'Related Projects';
+      } else {
+         $header = 'Related Posts';
+      }
+      return $header;
+   }
 endif;
--- public_html/wp-content/themes/bain-2020/functions/theme.php
+++ public_html/wp-content/themes/bain-2020/functions/theme.php
@@ -12,59 +12,14 @@
  * }
  * 
  */
-function baindesign324_related_blog_posts() {
-   global $post;  
-    $orig_post = $post;
 
-    // Only show related posts on single posts
-    if ( ! is_single() ) {
-       return;
-    }
+/**
+ * Related Posts Section Header
+ */
 
-   $custom_taxterms = wp_get_object_terms( 
-      $post->ID, 'post_tag', array(
-         'fields' => 'ids'
-      ) 
-   );
-    
-   $args = array(  
-       'post_type' => array(
-          'award',
-          'book',
-          'interview',
-          'project',
-          'post',
-          'talk',
-          'film',
-      ),
-      'post__not_in'			=> array( 
-         $post->ID 
-      ),  
-      'posts_per_page'		=> 3, 
-      'ignore_sticky_posts' 	=> 1,
-      'tax_query' => array(
-          array(
-              'taxonomy' => 'post_tag',
-              'field' => 'id',
-              'terms' => $custom_taxterms
-          )
-      ),
-   );  
+   function bd324_related_posts_section_header()
+   {
 
-   $my_query = new wp_query( $args );
-
-   if  ( $my_query->have_posts() ) : ?>
-      <div class="posts posts--related">
-         <div class="container">
-            <header><h3><?php _e('Similar, but also very different.','_baindesign'); ?></h3></header>
-            <div class="posts__wrapper">
-               <?php while( $my_query->have_posts() ) {
-                   $my_query->the_post();
-                  get_template_part('content', 'archive');
-               } ?>
-            </div><!-- ..container -->
-         </div><!-- ..container -->
-      </div><!-- #related-blog-posts .section -->	
-   <?php endif;					
-   $post = $orig_post; wp_reset_query();
-}
+      $header = 'Similar, but also different';
+      return $header;
+   }
