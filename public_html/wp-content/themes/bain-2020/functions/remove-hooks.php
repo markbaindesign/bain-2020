8ac5669e9dc17c24d3df2d2395da4e60bfaa1a74
markbain <markbaindesign@gmail.com>
2020-05-11 18:29:28 +0200

    Restore default fonts

--- public_html/wp-content/themes/bain-2020/functions/remove-hooks.php
+++ public_html/wp-content/themes/bain-2020/functions/remove-hooks.php
@@ -33,8 +33,6 @@ remove_action( 'wp_enqueue_scripts',   'bd324_enqueue_fp_styles',               
 remove_action('baindesign324_header_top', 'baindesign324_mmenu_toggle', 40);
 remove_action('baindesign324_header_top', 'baindesign324_mmenu_toggle_static', 40);
 remove_action('baindesign324_pre_header', 'baindesign324_menu_account', 50);
-remove_action( 'wp_head', 'bd324_typekit', 10 );
-remove_action('baindesign324_pre_header', 'baindesign324_menu_account', 50);
 remove_action('baindesign324_pre_header', 'baindesign324_pre_header_wrapper_close', 100);
 remove_action('baindesign324_pre_header', 'baindesign324_pre_header_wrapper_open', 10);
 remove_action('baindesign324_pre_header', 'baindesign324_social_links', 60);
