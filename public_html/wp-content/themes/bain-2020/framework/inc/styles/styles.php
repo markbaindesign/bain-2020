08c76f9619ebaa4b2452d194e5331d83c1aac7c7
markbain <markbaindesign@gmail.com>
2020-05-14 17:47:55 +0200

    Load versioned stylesheet

--- public_html/wp-content/themes/bain-2020/framework/inc/styles/styles.php
+++ public_html/wp-content/themes/bain-2020/framework/inc/styles/styles.php
@@ -2,12 +2,14 @@
 /**
  * baindesign324_enqueue_styles
  */
-if ( ! function_exists( 'baindesign324_enqueue_styles' ) ) :
-    function baindesign324_enqueue_styles() {
-        if ( !is_admin() ) { 
-            wp_enqueue_style( 'baindesign324-style', get_template_directory_uri() . '/style.css', array(), null );        
-        }
-    }
+if (!function_exists('baindesign324_enqueue_styles')) :
+   function baindesign324_enqueue_styles()
+   {
+      if (!is_admin()) {
+         $version = baindesign324_theme_version();
+         wp_enqueue_style('baindesign324-style', get_template_directory_uri() . '/style.' . $version . '.css', array(), null, false);
+      }
+   }
 endif;
 
 if ( ! function_exists( 'baindesign324_enqueue_animation_styles' ) ) :
